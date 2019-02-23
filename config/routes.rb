7353fdc0c93b2a4c04d0232d3a1a349945bfa3bf
Shaun Carland <scarland@justworks.com>
2019-02-22 21:18:12 -0500

    add constituent model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :constituents
   devise_for :represenatives
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
