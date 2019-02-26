8ea624a2e5635a935a46d7d1a3e53e4b8280b61d
Shaun Carland <scarland@justworks.com>
2019-02-26 17:12:17 -0500

    add current_person

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+  devise_group :person, contains: [:represenative, :constituent]
+  # before_action :authenticate_person! # Ensure someone is logged in
 end
