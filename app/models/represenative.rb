beb347237787197292223e065c4c8f1f2a53f3f2
Shaun Carland <thestarsareprojectors824@gmail.com>
2019-03-02 23:19:07 -0500

    Run rails destroy devise Represenatives

--- app/models/represenative.rb
+++
@@ -1,9 +0,0 @@
-class Represenative < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :bills
-  has_many :terms
-end
