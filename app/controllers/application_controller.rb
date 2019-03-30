914e6eddc5de0bd4d8aac76b041460bcec305f01
Shaun Carland <scarland@justworks.com>
2019-03-29 21:30:42 -0400

    add first and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:website, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:website, :phone_number, :first_name, :last_name])
   end
 end
