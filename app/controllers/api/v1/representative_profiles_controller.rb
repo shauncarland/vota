99bbeb93a1569584e0fe41b4f80ef3e7a1e67dc9
Shaun Carland <scarland@justworks.com>
2019-03-29 21:50:11 -0400

    Revert "Rename representatives_profiles to representative_profile"
    This reverts commit 15712ffa18b0fabcd90c9285344da53ea7bb510e.

--- app/controllers/api/v1/representative_profiles_controller.rb
+++ app/controllers/api/v1/representative_profiles_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class RepresentativeProfileController < ApplicationController
+    class RepresentativeProfilesController < ApplicationController
       def index
         rp = RepresentativeProfile.where(representative_id: params[:representative_id]).first
         render json: { representative_id: rp.id, representative_first_name: rp.representative.first_name }
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :representatives, only: [:index, :show] do
-        resources :representative_profile, only: [:index]
+        resources :representative_profiles, only: [:index, :show]
       end
     end
   end
