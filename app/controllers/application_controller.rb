950f93c2bc5e692ae546b922b59502a894a25a05
Shaun Carland <scarland@justworks.com>
2019-03-29 21:29:22 -0400

    fill in phone number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.for(:sign_up) << :website
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:website])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:website, :phone_number])
   end
-
 end
