cefbbd04337e2a0f014220416a88fdb12fb5450b
Shaun Carland <scarland@justworks.com>
2019-04-09 21:05:47 -0400

    Put in action

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,3 @@
 class Api::V1::BaseController < ActionController::Base
-  # before_action :authenticate_representative! # Ensure someone is logged in
+  before_action :authenticate_representative! # Ensure someone is logged in
 end
